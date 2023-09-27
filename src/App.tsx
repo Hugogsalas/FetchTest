59330a638780cb14b954ccb783961d0b82d0a779
Hugo Gomez <hugogsalas79@gmail.com>
2023-09-26 22:35:07 -0600

    fix: react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './views/login/component';
 import Home from './views/home/component';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/home',
-    element: <Home />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Login />,
+    },
+    {
+      path: '/home',
+      element: <Home />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' }
+);
 
 const App = () => {
   return <RouterProvider router={router} />;
